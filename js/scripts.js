2de7de694cba0a841361f3d96857880072b563d9
paulutenggario <47066367+paulutenggario@users.noreply.github.com>
2019-03-13 23:49:36 +0000

    index;about-us
    update index page, add about us page

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,18 @@ function myFunction() {
   // alert("Form submitted!");
   return;
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 250) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
